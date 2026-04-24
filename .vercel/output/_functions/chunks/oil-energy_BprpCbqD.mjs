9b80a1b7b4f15af20407d6046b00c45370721e54
Keller Scholl <keller.scholl@gmail.com>
2026-04-24 16:56:57 -0400

    Stop tracking .vercel build output

--- .vercel/output/_functions/chunks/oil-energy_BprpCbqD.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./oil-energy_BGL4r4pJ.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
--- .vercel/output/_functions/chunks/stocks_DWQP0Cba.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./stocks_Dj3Ht_yL.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
